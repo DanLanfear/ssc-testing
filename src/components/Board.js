066d82efdec079db5c81a36400cd8cd57441818f
Daniel Lanfear <lanfeadb@udmercy.edu>
2022-12-17 16:56:11 -0500

    formatted and added render functions

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,14 +1,38 @@
 import React from "react";
-import Test from "./Test"
+import Test from "./Test";
 
 class Board extends React.Component {
-    // constructor, which sets stat
+  // constructor, which sets stat
+  constructor(props) {
+    super(props);
+    this.state = {
+      tests: Array(10).fill(null),
+    };
+  }
 
-    // handleClick
-    
-    // render test function
+  // handleClick
 
-    // render
+  // render test function
+  renderTest(i) {
+    return <Test name={this.state.tests[i]} />;
+  }
+
+  // render
+  render() {
+    return (
+      <div>
+        {this.renderTest(0)}
+        {this.renderTest(2)}
+        {this.renderTest(3)}
+        {this.renderTest(4)}
+        {this.renderTest(5)}
+        {this.renderTest(6)}
+        {this.renderTest(7)}
+        {this.renderTest(8)}
+        {this.renderTest(9)}
+      </div>
+    );
+  }
 }
 
-export default Board
+export default Board;
--- src/components/Test.js
+++ src/components/Test.js
@@ -1,16 +1,14 @@
 import React from "react";
 
 function Test(props) {
-    // shows a test entity
-    // has name of tester, time left, button to end test
-    return(
-        <div>
-            {props.name}   {props.time}
-            <button>
-
-            </button>
-        </div>
-    );
+  // shows a test entity
+  // has name of tester, time left, button to end test
+  return (
+    <div>
+      {props.name} {props.time}
+      <button></button>
+    </div>
+  );
 }
 
-export default Test
+export default Test;
