9788c03cb8d1e50826f5959e633bd7a679e3aaae
Daniel Lanfear <lanfeadb@udmercy.edu>
2022-12-18 14:46:49 -0500

    Fine tuned some styling

--- src/components/Test.js
+++ src/components/Test.js
@@ -5,12 +5,16 @@ function Test(props) {
   // shows a test entity
   // has name of tester, time left, button to end test
   return (
-    <div className="row test-entity">
-      <div className="col">Test ID: {props.test.id}</div>
-      <div className="col">Tester Name: {props.test.name}</div>
-      <div className="col">Time Limit (Minutes): {props.test.timeLimit}</div>
-      <div className="col">
-        <Button variant="danger">delete</Button>
+    <div className="row test-entity m-4">
+      <div className="col test-info">Test ID: {props.test.id}</div>
+      <div className="col test-info">Tester Name: {props.test.name}</div>
+      <div className="col test-info">
+        Time Limit (Minutes): {props.test.timeLimit}
+      </div>
+      <div className="col test-info">
+        <Button className="delete-btn" variant="danger">
+          delete
+        </Button>
       </div>
     </div>
   );
