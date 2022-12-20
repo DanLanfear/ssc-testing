9d1f9cbe563bd08f1441527d64f342d2fe40aaa0
Daniel Lanfear <lanfeadb@udmercy.edu>
2022-12-19 20:54:47 -0500

    Fixed banner at top of screen

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="main-container">
-      <header role="banner"></header>
-      <h1 className="title">Student Success Center Test Monitoring</h1>
+      <div className="banner">
+        <h1 className="title">Student Success Center Test Monitoring</h1>
+      </div>
       <Board />
       {/* button for adding a test */}
     </div>
